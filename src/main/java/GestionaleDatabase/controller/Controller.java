332f810f459fa4b02806a1190b855ea5829b0ccd
Eres <russovincenzo820@gmail.com>
2025-09-11 15:47:41 +0200

    fix corse

--- src/main/java/GestionaleDatabase/controller/Controller.java
+++ src/main/java/GestionaleDatabase/controller/Controller.java
@@ -5,6 +5,7 @@ import GestionaleDatabase.service.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class Controller {
     private AdministratorService administratorService;
 
     @PostMapping("/login")
-
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<AdministratorEntity> login(@RequestParam String username, @RequestParam String password) {
         AdministratorEntity utente = new AdministratorEntity();
         utente = administratorService.login(username, password);
@@ -28,7 +29,7 @@ public class Controller {
     }
 
     @PostMapping("/registrazione")
-
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<AdministratorEntity> registrazione(@RequestParam String nome, @RequestParam String cognome, @RequestParam(required = false) String email, @RequestParam(required = false) String cellulare) {
         AdministratorEntity admin = new AdministratorEntity();
         admin = administratorService.registrazione(nome, cognome, email, cellulare);
